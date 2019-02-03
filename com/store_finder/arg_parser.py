d4a811e8026f9cc972fd5cb043d653ab558589ba
Ian Mrak <ianmrak@gmail.com>
2019-02-02 20:57:06 -0800

    [Support] Update help descriptions for arguments.

--- com/store_finder/arg_parser.py
+++ com/store_finder/arg_parser.py
@@ -4,8 +4,8 @@ import argparse
 def parse_args(args):
     parser = argparse.ArgumentParser()
     addressTypes = parser.add_mutually_exclusive_group()
-    addressTypes.add_argument('--zip', dest='address')
-    addressTypes.add_argument('--address', dest='address')
+    addressTypes.add_argument('--zip', dest='address', help='Example: \'Main St., USA\'')
+    addressTypes.add_argument('--address', dest='address', help='Example: \'123456\'')
     parser.add_argument('--output', default='text', choices=['text', 'json'])
     parser.add_argument('--units', default='imperial', choices=['imperial', 'metric', 'km', 'miles'])
     parsed_args = parser.parse_args(args)
